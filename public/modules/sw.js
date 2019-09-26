1ed39ca50a53fc9a2147ed33025186b58e8ae645
Marcus Baer <marcus.baer@publicispixelpark.de>
2019-09-26 09:52:39 +0200

    use templates for vue

---
+++ public/modules/sw.js
@@ -0,0 +1,17 @@
+'use strict';
+
+window.addEventListener('beforeinstallprompt', event => event.prompt())
+
+if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+        console.log("Active service worker found, no need to register");
+    } else {
+        // Register the service worker
+        navigator.serviceWorker.register("/service-worker.js", {
+            scope: "/"
+        })
+        .then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+}
---
+++ public/sw/cache.js
@@ -0,0 +1,71 @@
+const CACHE = "play-v190926";
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE).then(function(cache) {
+            cache.addAll([
+                // cache resources that are used later on
+            ]);
+            return cache.addAll([
+                // cache resources that are required directly
+                'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css',
+                'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.min.js',
+                'https://cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.esm.browser.js',
+                // '/modules/tpl/App.html',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.method !== "GET") return;
+    //console.log("FETCH", event.request.url);
+
+    // if (event.request.url.indexOf('tpl/')>0 && event.request.url.endsWith('.html')) {
+
+    //     // convert templates for Vue
+    //     event.respondWith(
+    //         fetch(event.request.url)
+    //             .then((response) => response.text())
+    //             .then((body) => {
+    //                 // Export the response body as a JavaSript string.
+    //                 // The response body has to be sanitized before turning it
+    //                 // into JavaScript code.
+    //                 // Credits: https://stackoverflow.com/a/22837870
+    //                 const newBody = `export default "${JSON.stringify(body).slice(1, -1)}"`
+    //                 // Replace the original response with an ES6 module
+    //                 return new Response(newBody, {
+    //                     headers: new Headers({
+    //                         'Content-Type': 'application/javascript'
+    //                     })
+    //                 })
+    //             })
+    //     )
+
+    // } else {
+        // other files from cache first, then request (no caching)
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (event.request.url.indexOf('tpl/')>0 && event.request.url.endsWith('.html')) {
+                    return response || fetch(event.request.url)
+                        .then((response) => response.text())
+                        .then((body) => {
+                            // Export the response body as a JavaSript string.
+                            // The response body has to be sanitized before turning it
+                            // into JavaScript code.
+                            // Credits: https://stackoverflow.com/a/22837870
+                            const newBody = `export default "${JSON.stringify(body).slice(1, -1)}"`
+                            // Replace the original response with an ES6 module
+                            return new Response(newBody, {
+                                headers: new Headers({
+                                    'Content-Type': 'application/javascript'
+                                })
+                            })
+                        })
+                } else {
+                    return response || fetch(event.request);
+                }
+            })
+        );
+    // }   
+});
