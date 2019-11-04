8c34e7c941406fc4cf26ef75b8e38b0a60d7eb86
Marcus Baer <marcus.baer@publicispixelpark.de>
2019-11-04 11:21:35 +0100

    use api for service

--- public/modules/services.js
+++ public/modules/services.js
@@ -2,19 +2,26 @@
 
 import Person from './person.js';
 
-function createPersonsList() {
+// function createPersonsList() {
 
-    return [
-        Person.create({type: 'adult', name: 'Bernd', age: 36, email: 'bernd@family.com', phone: '+49 733 48943 448', children: ['Emilia'], citation: 'Wir sind so froh, dass wir noch ein E bekommen haben.'}),
-        Person.create({type: 'child', name: 'Emilia', age: 8, email: 'emilia@family.com', favorite: 'Teddy', citation: 'Ohne das E hätte ich nie reden gelernt. Mein Name würde auch nicht funktionieren.'}),
-    ];
-}
+//     return [
+//         Person.create({type: 'adult', name: 'Bernd', age: 36, email: 'bernd@family.com', phone: '+49 733 48943 448', children: ['Emilia'], citation: 'Wir sind so froh, dass wir noch ein E bekommen haben.'}),
+//         Person.create({type: 'child', name: 'Emilia', age: 8, email: 'emilia@family.com', favorite: 'Teddy', citation: 'Ohne das E hätte ich nie reden gelernt. Mein Name würde auch nicht funktionieren.'}),
+//     ];
+// }
 
 export const PersonsService = {
     async load (config = {}) {
-        return createPersonsList()
+        // return createPersonsList()
         // return new Promise((resolve, reject) => {})
-        // return fetch(`persons.json`).then(response => response.json())
+
+        // upcoming I: errors or warnings might be collected in an array to be displayed somewhere...
+        // upcoming II: fetch should have then and catch and finally blocks
+        return fetch(`api/persons.json`).then(response => response.json())
+        .then(json => json.result)
+        .then(result => result.map(item => Person.create(item)))
+        .catch(err => { console.error(err); return []; })
+        .finally(() => console.log('Persons loaded'))
     },
 }
 
