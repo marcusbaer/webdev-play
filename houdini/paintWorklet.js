6db04f1d1655bda54a0d2487430d295adfb95b5c
Marcus Baer <marcus.baer@publicispixelpark.de>
2020-07-10 10:35:17 +0200

    Houdini presentation

--- houdini/paintWorklet.js
+++ houdini/paintWorklet.js
@@ -1,12 +1,14 @@
-registerPaint("paintWorketExample", class {
+registerPaint("paintWorkletExample", class {
     static get inputProperties() { return ["--myVariable"]; }
     static get inputArguments() { return ["<color>"]; }
     static get contextOptions() { return {alpha: true}; }
   
     paint(ctx, size, properties, args) {
-      console.log(ctx)
-      console.log(size)
-      console.log(properties)
-      console.log(args)
+      console.group('Paint API')
+      console.log('Context', ctx)
+      console.log('Size', size)
+      console.log('Properties', properties)
+      console.log('Arguments', args)
+      console.groupEnd('Paint API')
     }
   });
