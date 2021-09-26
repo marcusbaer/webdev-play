c8414887b847dcbe7286234bff7e0c0a171e85c2
Marcus Baer <marcus.baer@publicispixelpark.de>
2021-09-26 23:39:12 +0200

    clean up snippets and remove test runner

--- modern-web/snippets/join.test.js
+++
@@ -1,6 +0,0 @@
-import { expect } from '@esm-bundle/chai'
-
-it('joins an array to a string', () => {
-  expect(['a', 'b'].join('-')).to.equal('a-b')
-  expect(['a', 'b'].join('')).to.equal('ab')
-})
